e6ae6b6492b2dbebdcfc6c480292820b3d911d9a
shehabislam99 <shihabkhanahab@gmail.com>
2026-04-09 22:41:40 +0530

    copy and string learn

--- javaScriptBasic.js
+++ javaScriptBasic.js
@@ -1,11 +1,23 @@
 var x = 5;
 var y = 10;
-var z = x + y;
+var z = x + y; //add numbers
+console.log(z);
+var z = x - y; //subtract numbers
 console.log(z);
 
-let  a = 20;
-const b = 30
-console.log(a+b);
+let i = 70;
+let j = 30;
+let k = i + j; //usinng  let for variable declaration
+console.log(k);
+
+const  a = 20;
+const b = 30 
+ const f = a+b//usinng const for variable declaration
+console.log(f);
+
+let c = 20;
+const d = c; //Copying value of c to d
+console.log(d);
 
 const name = "shehab";
-console.log( name);
+console.log( name); //string variable declaration and printing
