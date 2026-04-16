ccef87054c89689b07cf4041c5522591c1930fe7
shehabislam99 <shihabkhanahab@gmail.com>
2026-04-17 03:17:02 +0530

    Conversion Operation

---
+++ 03_ConversionOperation.js
@@ -0,0 +1,67 @@
+let score = "shehab"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"shehab" => true; "0" => true
+//"" => false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//***************Opreation****************//
+let value = 3
+let negValue = -value
+// console.log(negValue)
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**8);
+// console.log(2/3);
+// console.log(7%3);
+
+let str1 = "shehab "
+let str2 = "Aslam"
+
+let str3 = str1  + str2
+// console.log(str3);
+
+// console.log("1" + 2) => 12
+// console.log(1 + "2") => 12
+// console.log("1" + 2 + 2); => 122
+// console.log(1 + 2 + "2"); => 32
+// console.log((3 + 4) * (5 % 3)); => 14
+//console.log(+true); => 1
+// console.log(+""); => 0
+
+let num1, num2, num3
+ num1 = num2 = num3 = 2 + 2
+
+ let gameCounter = 100
+ ++gameCounter;
+ console.log(gameCounter);
+ 
+
+
+
+
+
+
+
+
+
